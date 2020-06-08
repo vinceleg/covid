e07ab5bc118f8c26ec3b0912a3ce3d34afec3e37
Vincent L <vincentliang_01@hotmail.com>
2020-06-07 21:12:34 -0700

    Updates

---
+++ precache-manifest.4369bc99828e93734badc45bf2baae8d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ffc189f814828d8b78d2296907c8e7",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "bcf0f2cd98996cdc727f",
+    "url": "/covid/static/css/main.1195b738.chunk.css"
+  },
+  {
+    "revision": "003b7b8b9ee0f0f9593c",
+    "url": "/covid/static/js/2.655eb455.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid/static/js/2.655eb455.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcf0f2cd98996cdc727f",
+    "url": "/covid/static/js/main.7cf5386f.chunk.js"
+  },
+  {
+    "revision": "275dbaca090ef9041c1a",
+    "url": "/covid/static/js/runtime-main.a0797417.js"
+  },
+  {
+    "revision": "7f8d101424f2300c9c856fef48178f12",
+    "url": "/covid/static/media/deaths.7f8d1014.jpg"
+  },
+  {
+    "revision": "9115c5125add43ef7c356bfcb2e62411",
+    "url": "/covid/static/media/infected.9115c512.jpg"
+  },
+  {
+    "revision": "224c1d149fe260b051f757a65789157d",
+    "url": "/covid/static/media/recovered.224c1d14.jpg"
+  },
+  {
+    "revision": "74a9d93ec963b36c63f3565874dc2b2b",
+    "url": "/covid/static/media/title-image.74a9d93e.png"
+  }
+]);
